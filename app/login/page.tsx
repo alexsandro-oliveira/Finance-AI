4d992aae60dc02324b5fd41768b714be0a8e047a
Alexs <ale.osantos@hotmail.com>
2025-01-15 20:43:32 +0000

    feat: add login page

---
+++ app/login/page.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import { LogInIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
+
+const LoginPage = async () => {
+  return (
+    <div className="grid h-full grid-cols-2">
+      {/* Left side */}
+      <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
+        <Image
+          src="/logo.svg"
+          alt="Finance AI"
+          width={163}
+          height={39}
+          className="mb-8"
+        />
+        <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
+        <p className="mb-8 text-muted-foreground">
+          A Finance AI é uma plataforma de gestão financeira que utiliza IA para
+          monitorar suas movimentações, e oferecer insights personalizados,
+          facilitando o controle do seu orçamento.
+        </p>
+
+        <Button variant="outline">
+          <LogInIcon className="mr-2" />
+          Fazer login ou criar conta
+        </Button>
+      </div>
+
+      {/* Right side */}
+      <div className="relative h-full w-full">
+        <Image
+          src="/login.png"
+          alt="Faça Login"
+          fill
+          className="object-cover"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;

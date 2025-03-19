e12889fcc615df989911329061d98a3b5b2bec5f
Alexs <ale.osantos@hotmail.com>
2025-03-19 11:40:38 +0000

    feat: add sidebar-sheet and mobile menu and in the navbar

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -4,6 +4,10 @@ import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Sheet, SheetTrigger } from "./ui/sheet";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+import SidebarSheet from "./sidebar-sheet";
 
 const Navbar = () => {
   const pathname = usePathname();
@@ -12,39 +16,54 @@ const Navbar = () => {
       {/* ESQUERDA */}
       <div className="flex items-center gap-10">
         <Image src="/logo.svg" width={173} height={39} alt="Finance AI" />
-        <Link
-          href="/"
-          className={
-            pathname === "/"
-              ? "font-bold text-primary"
-              : "text-muted-foreground"
-          }
-        >
-          Dashboard
-        </Link>
-        <Link
-          href="/transactions"
-          className={
-            pathname === "/transactions"
-              ? "font-bold text-primary"
-              : "text-muted-foreground"
-          }
-        >
-          Transações
-        </Link>
-        <Link
-          href="/subscription"
-          className={
-            pathname === "/subscription"
-              ? "font-bold text-primary"
-              : "text-muted-foreground"
-          }
-        >
-          Assinatura
-        </Link>
+        <div className="flex items-center gap-10 max-md:hidden">
+          <Link
+            href="/"
+            className={
+              pathname === "/"
+                ? "font-bold text-primary"
+                : "text-muted-foreground"
+            }
+          >
+            Dashboard
+          </Link>
+          <Link
+            href="/transactions"
+            className={
+              pathname === "/transactions"
+                ? "font-bold text-primary"
+                : "text-muted-foreground"
+            }
+          >
+            Transações
+          </Link>
+          <Link
+            href="/subscription"
+            className={
+              pathname === "/subscription"
+                ? "font-bold text-primary"
+                : "text-muted-foreground"
+            }
+          >
+            Assinatura
+          </Link>
+        </div>
       </div>
       {/* DIREITA */}
-      <UserButton showName />
+      <div className="flex items-center max-md:hidden">
+        <UserButton showName />
+      </div>
+
+      <div className="hidden max-md:block">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SidebarSheet />
+        </Sheet>
+      </div>
     </nav>
   );
 };
